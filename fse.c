1108a480391d3853d3a0ef05fffc344105840421
Nima Naghibi <dude.nimanaghibi@gmail.com>
2026-06-07 10:46:41 +0330

    chore: initial setup

---
+++ fse.c
@@ -0,0 +1,2 @@
+/* fse.c */
+#include "fse.h"
---
+++ fse.h
@@ -0,0 +1,8 @@
+/* fse.h*/
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arcfour.h>
+
+int main (int, char**); 
